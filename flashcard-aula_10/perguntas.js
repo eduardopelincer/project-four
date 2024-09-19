69d0948e2180c2501197765fc714966906f46222
eduardo. <140827650+eduardopelincer@users.noreply.github.com>
2024-09-19 14:21:53 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'matematica',
+    '2=89?',
+    '91'
+)
+
+criaCartao(
+    'Geografia',
+    'quantos hab tem sti?',
+    '24.262'
+)
+
+criaCartao(
+    'geografia',
+    'quem fez a clt?',
+    'getulio vargas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
